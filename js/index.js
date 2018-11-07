2ce0efc81a508690a69cb9b951d0148befb4c9af
VladLaz27 <39669227+VladLaz27@users.noreply.github.com>
2018-11-07 09:15:37 +0200

    Polomaniy slider

--- js/index.js
+++ js/index.js
@@ -4,3 +4,16 @@
     return false;
   });
 });
+
+
+
+    var swiper = new Swiper('.swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
